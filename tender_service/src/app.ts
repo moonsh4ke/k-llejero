2eea335b1cd6d23b6e854920a245c781c4bd8a80
Martin Cespedes <mcespedescubillos@gmail.com>
2024-04-23 19:03:39 -0400

    chore(tender): added asyncWrapper function

--- tender_service/src/app.ts
+++ tender_service/src/app.ts
@@ -4,7 +4,7 @@ import { Tender, TenderRawDocType, TenderDoc } from './models/tender'
 import { Document, FilterQuery, Query, QueryWithHelpers } from 'mongoose';
 import { z } from "zod";
 import CustomValidationError from '@sn1006/common/build/errors/validation-error';
-import { errorHandler } from '@sn1006/common';
+import { errorHandler, asyncWrapper } from '@sn1006/common';
 import morgan from 'morgan';
 
 const app = express();
@@ -12,36 +12,44 @@ app.use(json());
 app.use(morgan('tiny'));
 app.set('trust proxy', true);
 
-app.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const tender = await Tender.findById(id);
-  if(!tender)
-    return res.status(404);
+app.get(
+  '/:code',
+  asyncWrapper(async (req, res) => {
+      const { code } = req.params;
+      const tender = await Tender.findOne({ code });
+      if(!tender)
+        return res.status(404);
 
-  return res.status(200).send(tender)
-})
+      return res.status(200).send(tender)
+    }
+  )
+)
 
-app.get('/', async (req, res) => {
-  const { filter: filterJson } = req.query;
+app.get(
+  '/',
+  asyncWrapper(async (req, res) => {
+      const { filter: filterJson } = req.query;
 
-  let tenders: Document<TenderDoc>[];
+      let tenders: Document<TenderDoc>[];
 
-  if (filterJson) {
-    const filters = JSON.parse(filterJson as string);
+      if (filterJson) {
+        const filters = JSON.parse(filterJson as string);
 
-    const filtersParsed = Object.keys(filters).map(key => {
-      let parsedFilter: Record<string, any> = {}
-      const regex = new RegExp(filters[key], 'i');
-      parsedFilter[key] = regex;
-      return parsedFilter;
-    });
-    tenders = await Tender.find({$and: filtersParsed});
-  }
-  else
-    tenders = await Tender.find({});
+        const filtersParsed = Object.keys(filters).map(key => {
+          let parsedFilter: Record<string, any> = {}
+          const regex = new RegExp(filters[key], 'i');
+          parsedFilter[key] = regex;
+          return parsedFilter;
+        });
+        tenders = await Tender.find({$and: filtersParsed});
+      }
+      else
+        tenders = await Tender.find({});
 
-  return res.status(200).send(tenders);
-})
+      return res.status(200).send(tenders);
+    }
+  )
+)
 
 app.use(errorHandler);
 
